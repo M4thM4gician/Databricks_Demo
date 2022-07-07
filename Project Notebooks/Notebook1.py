7567fa5b7d21efbe839aeee6c36f67814d53ef18
ruhlandct02@gmail.com <ruhlandct02@gmail.com>
2022-07-07 22:07:08 +0000

    initial commit

---
+++ Project Notebooks/Notebook1.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %md # This is the first notebook
+# MAGIC This notebook will be used to do some awesome data science!!!!
+
+# COMMAND ----------
+
+# MAGIC %RUN 'Notebook2.python'
+
+# COMMAND ----------
+
+var1 = 5
+var2 = 12
+print(var1 + var2)
---
+++ Project Notebooks/Notebook2.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %md # Utilities
+# MAGIC This notebook contains utilities to be used by other notebooks
+
+# COMMAND ----------
+
+def my_func():
+    print("get funky!")
